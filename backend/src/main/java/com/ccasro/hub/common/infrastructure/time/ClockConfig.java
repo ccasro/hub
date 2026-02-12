5908483ac410b88b256231ecc76eeb237fe64c97
Cesar <ccasro@gmail.com>
2026-02-12 17:02:06 +0100

    chore(format): reformat with Spotless 3.2.1

--- backend/src/main/java/com/ccasro/hub/common/infrastructure/time/ClockConfig.java
+++ backend/src/main/java/com/ccasro/hub/common/infrastructure/time/ClockConfig.java
@@ -1,14 +1,13 @@
 package com.ccasro.hub.common.infrastructure.time;
 
+import java.time.Clock;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.time.Clock;
-
 @Configuration
 public class ClockConfig {
-    @Bean
-    public Clock clock() {
-        return Clock.systemUTC();
-    }
+  @Bean
+  public Clock clock() {
+    return Clock.systemUTC();
+  }
 }
