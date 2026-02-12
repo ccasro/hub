692e82fde17c751b207c17ac8a464abca8aca533
Cesar <ccasro@gmail.com>
2026-02-12 12:21:10 +0100

    feat(common): add global exception handler with ProblemDetail

---
+++ backend/src/main/java/com/ccasro/hub/common/api/error/GlobalExceptionHandler.java
@@ -0,0 +1,52 @@
+package com.ccasro.hub.common.api.error;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.net.URI;
+import java.time.Instant;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ProblemDetail handleGenericException(
+            Exception ex,
+            HttpServletRequest request
+    ) {
+
+        log.error("Unhandled exception at {}", request.getRequestURI(),ex);
+
+        return problem(HttpStatus.INTERNAL_SERVER_ERROR, "/errors/internal-server-error","Internal Server Error", "Unexpected error occurred", request);
+    }
+
+
+   private ProblemDetail problem(
+           HttpStatus status,
+           String type,
+           String title,
+           String detail,
+           HttpServletRequest request
+   ) {
+        ProblemDetail pd = ProblemDetail.forStatus(status);
+
+        pd.setType(URI.create(type));
+        pd.setTitle(title);
+        pd.setDetail(detail);
+        pd.setInstance(URI.create(request.getRequestURI()));
+        pd.setProperty("timestamp", Instant.now().toString());
+
+       String traceId = MDC.get("traceId");
+       if (traceId != null && !traceId.isBlank()) {
+           pd.setProperty("traceId", traceId);
+       }
+
+        return pd;
+   }
+}
