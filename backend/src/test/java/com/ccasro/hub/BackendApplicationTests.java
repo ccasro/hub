a0f897dc83653b87673fb75caa5e45eef804ff57
ccasro <ccasro@gmail.com>
2026-02-15 08:52:24 +0100

    test: refactor BackendApplicationTests to extend from BaseIT

--- backend/src/test/java/com/ccasro/hub/BackendApplicationTests.java
+++ backend/src/test/java/com/ccasro/hub/BackendApplicationTests.java
@@ -1,12 +1,10 @@
 package com.ccasro.hub;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
-class BackendApplicationTests {
+class BackendApplicationTests extends BaseIT {
 
   @Test
   void contextLoads() {}
